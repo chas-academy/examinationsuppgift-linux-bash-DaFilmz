c877eebc371a405a28f9d82ff920df92f4c06fd1
DaFilmz <simon9877@live.se>
2026-05-07 12:46:14 +0000

    Root kontroll och mappar

--- create_users.sh
+++ create_users.sh
@@ -2,9 +2,24 @@
 
 #kontrollera att användare har root / är Superuser
 
-if ["$EUID" -ne 0]; then #Noll är Superuser.
-	echo "Du har inte tillgång till programmet. (Superuser)"
+echo "Start."
+
+if [ "$EUID" -ne 0 ];
+	then echo "Programmet måste köras som root. (Superuser)"
 	exit 1
 fi
 
+for user in "$@"; do
+	if id "$user" &>/dev/null; then
+		echo "Användarnamntet '$user' finns redan i listan."
+		continue
+	fi
+	
+	useradd -m "$user"
+	echo "Användare $user har skapats"
+	#Här skapar vi filer och ger de läs och skriv rättigheter till den nya användaren.
+	mkdir -p "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
+	chmod 700 "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
+done
 
+echo "Klart."
