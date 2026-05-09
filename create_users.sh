128c1103b6c2bd5dec5aa335026c4a0cbeb23f0d
DaFilmz <simon9877@live.se>
2026-05-09 06:56:12 +0000

    Moved exsisitng users out of loop

--- create_users.sh
+++ create_users.sh
@@ -8,7 +8,7 @@ if [ "$EUID" -ne 0 ];
 	exit 1
 fi
 
-
+EXSISTING_USERS=$(cut -d: -f1 /etc/passwd)
 
 #for loop igenom alla användare som blivit givet.
 for user in "$@"; do
@@ -18,7 +18,6 @@ for user in "$@"; do
 	fi
 	#User shortcut var.
 	USER_DIR="/home/$user"
-	EXSISTING_USERS=$(cut -d: -f1 /etc/passwd)
 
 	useradd -m "$user"
 	echo "Användare $user har skapats"
@@ -29,9 +28,9 @@ for user in "$@"; do
 	#skapar välkoms fil.
 	WELCOME_FILE="$USER_DIR/welcome.txt"
 
-	#Lägger in text i rad 1.
+	#Lägger in text i rad.
 	echo "Välkommen $user" > "$WELCOME_FILE"
-
+	echo "Användare som redan finns:" >> "$WELCOME_FILE"
 	echo "$EXSISTING_USERS" >> "$WELCOME_FILE"
 
 	#Här ger vi rättigheter till rätt användare.
